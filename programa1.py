a1aad5806c7855f5889394858a19e4e3a84ea5b1
EGameiro <eduardo@digiplay.net.br>
2020-02-07 20:04:56 -0300

    Esssa foi a ultima atualização

--- programa1.py
+++ programa1.py
@@ -3,3 +3,13 @@ print("Ola....")
 #####
 print ("como vai você gostosão...?")
 ## sei sim
+# Tudo eu esta suspenso
+# Tudo eu esta suspenso
+# Tudo eu esta suspenso
+# Tudo eu esta suspenso
+# Tudo eu esta suspenso
+# Tudo eu esta suspenso
+# Tudo eu esta suspenso
+# Tudo eu esta suspenso# Tudo eu esta suspenso
+# Tudo eu esta suspenso
+# Tudo eu esta suspenso# Tudo eu esta suspenso# Tudo eu esta suspenso
